8e0191753561aaffc58063894a0ee0a1552448c1
Luca Wehrstedt <lcw@fb.com>
2021-12-17 04:23:45 -0800

    Prune old conda packages

--- packaging/conda.sh
+++ packaging/conda.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -ex
 
-export PYTORCH_BUILD_PREFIX="py${PYTHON_VERSION}_cuda${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}_cudnn"
-export PYTORCH_VERSION=$(conda search --json 'pytorch[channel=pytorch-nightly]' | python -c "import json, sys; d = json.load(sys.stdin); sys.stdout.write(''.join(sorted(p['version'] + '\n' for p in d['pytorch'] if p['build'].startswith('${PYTORCH_BUILD_PREFIX}'))[-1]))")
+export FAIRRING_BUILD="py${PYTHON_VERSION}_cuda${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}"
+
+export PYTORCH_VERSION=$(conda search --json 'pytorch[channel=pytorch-nightly]' | python -c "import json, sys; d = json.load(sys.stdin); sys.stdout.write(''.join(sorted(p['version'] + '\n' for p in d['pytorch'] if p['build'].startswith('${FAIRRING_BUILD}_cudnn'))[-1]))")
 export DATE=$(echo ${PYTORCH_VERSION} | sed -re 's/^.*\.dev([0-9]+)$/\1/')
 
 export FAIRRING_LATEST_TAG=$(git tag -l --sort=version:refname 'v*' | tail -n 1 | sed -re 's/^v//')
 export FAIRRING_VERSION="${FAIRRING_LATEST_TAG}.dev${DATE}"
 
-if conda search --json "fairring==${FAIRRING_VERSION}[channel=fairring,build=py${PYTHON_VERSION}_cuda${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}]"; then
+if conda search --json "fairring==${FAIRRING_VERSION}[channel=fairring,build=${FAIRRING_BUILD}]"; then
     exit 0
 fi
 
@@ -30,3 +31,7 @@ kill $SPAMMER_PID
 
 conda install -yq anaconda-client
 anaconda -t "${ANACONDA_TOKEN}" upload -u fairring --label main --force --no-progress /opt/conda/conda-bld/linux-64/fairring-*.tar.bz2
+
+for version in `conda search --json "fairring[channel=fairring,build=${FAIRRING_BUILD}]" | python -c "import json, sys; d = json.load(sys.stdin); sys.stdout.write(''.join(sorted(p['version'] + '\n' for p in d['fairring'])[:-1]))"`; do
+  anaconda -t "${ANACONDA_TOKEN}" remove --force fairring/fairring/${version}/linux-64/fairring-${version}-${FAIRRING_BUILD}.tar.bz2;
+done

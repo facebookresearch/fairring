d8d2bdf8b0f73b13567116c80fb74b7185f0055b
Luca Wehrstedt <lcw@fb.com>
2021-12-21 01:01:06 -0800

    Simplify conda build script

--- packaging/conda.sh
+++ packaging/conda.sh
@@ -3,7 +3,7 @@ set -ex
 
 export FAIRRING_BUILD="py${PYTHON_VERSION}_cuda${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}"
 
-export PYTORCH_VERSION=$(conda search --json 'pytorch[channel=pytorch-nightly]' | python -c "import json, sys; d = json.load(sys.stdin); sys.stdout.write(''.join(sorted(p['version'] + '\n' for p in d['pytorch'] if p['build'].startswith('${FAIRRING_BUILD}_cudnn'))[-1]))")
+export PYTORCH_VERSION=$(conda search --json 'pytorch[channel=pytorch-nightly]' | python -c "import json, sys; d = json.load(sys.stdin); p = max((p for p in d['pytorch'] if p['build'].startswith('${FAIRRING_BUILD}_cudnn')), key=lambda p: p['timestamp']); sys.stdout.write(p['version'] + '\n')")
 export DATE=$(echo ${PYTORCH_VERSION} | sed -re 's/^.*\.dev([0-9]+)$/\1/')
 
 export FAIRRING_LATEST_TAG=$(git tag -l --sort=version:refname 'v*' | tail -n 1 | sed -re 's/^v//')
@@ -21,12 +21,7 @@ fi
 export CUDA_HOME=/usr/local/cuda-${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}
 export PATH="$CUDA_HOME/bin:$PATH"
 
-conda install -c defaults -c $CUDATOOLKIT_CHANNEL -c pytorch -c pytorch-nightly pytorch==$PYTORCH_VERSION cudatoolkit=${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION} --download-only --yes
-
-for i in `seq 30`; do sleep 60; echo "Still going..."; done &
-SPAMMER_PID=$!
-conda build -c defaults -c $CUDATOOLKIT_CHANNEL -c pytorch -c pytorch-nightly -c conda-forge --no-anaconda-upload --python "$PYTHON_VERSION" packaging/fairring
-kill $SPAMMER_PID
+conda build -c defaults -c $CUDATOOLKIT_CHANNEL -c pytorch -c pytorch-nightly --no-anaconda-upload --python "$PYTHON_VERSION" packaging/fairring
 
 conda install -yq anaconda-client
 anaconda -t "${ANACONDA_TOKEN}" upload -u fairring --label main --force --no-progress /opt/conda/conda-bld/linux-64/fairring-*.tar.bz2

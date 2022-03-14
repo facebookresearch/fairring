0cfe7d6e32cd0da48a32ef86cad2116d35981fc6
Luca Wehrstedt <lcw@fb.com>
2022-03-14 07:16:54 -0700

    Build against stable PyTorch-1.11

--- packaging/conda.sh
+++ packaging/conda.sh
@@ -3,12 +3,10 @@ set -ex
 
 FAIRRING_BUILD="py${PYTHON_VERSION}_cuda${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}"
 
-PYTORCH_VERSION_AND_MD5=$(conda search --json 'pytorch[channel=pytorch-test]' | python -c "import json, sys; d = json.load(sys.stdin); p = max((p for p in d['pytorch'] if p['build'].startswith('${FAIRRING_BUILD}_cudnn')), key=lambda p: p['timestamp']); sys.stdout.write(p['version'] + ' ' + p['md5'] + '\n')")
-PYTORCH_VERSION=$(echo $PYTORCH_VERSION_AND_MD5 | awk '{ print $1 }')
-PYTORCH_MD5=$(echo $PYTORCH_VERSION_AND_MD5 | awk '{ print substr($2,1,8) }')
+PYTORCH_VERSION=$(conda search --json 'pytorch[channel=pytorch]' | python -c "import json, sys; d = json.load(sys.stdin); p = max((p for p in d['pytorch'] if p['build'].startswith('${FAIRRING_BUILD}_cudnn')), key=lambda p: p['timestamp']); sys.stdout.write(p['version'] + '\n')")
 
 FAIRRING_LATEST_TAG=$(git tag -l --sort=version:refname 'v*' | tail -n 1 | sed -re 's/^v//')
-FAIRRING_VERSION="${FAIRRING_LATEST_TAG}.dev${PYTORCH_MD5}"
+FAIRRING_VERSION="${FAIRRING_LATEST_TAG}"
 
 if conda search --json "fairring==${FAIRRING_VERSION}[channel=fairring,build=${FAIRRING_BUILD}]"; then
     exit 0
@@ -25,7 +23,7 @@ export FAIRRING_LATEST_TAG
 export CUDA_HOME=/usr/local/cuda-${CUDA_MAJOR_VERSION}.${CUDA_MINOR_VERSION}
 export PATH="$CUDA_HOME/bin:$PATH"
 
-conda build -c defaults -c $CUDATOOLKIT_CHANNEL -c pytorch -c pytorch-test --no-anaconda-upload --python "$PYTHON_VERSION" packaging/fairring
+conda build -c defaults -c $CUDATOOLKIT_CHANNEL -c pytorch -c pytorch --no-anaconda-upload --python "$PYTHON_VERSION" packaging/fairring
 
 conda install -yq anaconda-client
 anaconda -t "${ANACONDA_TOKEN}" upload -u fairring --label main --force --no-progress /opt/conda/conda-bld/linux-64/fairring-*.tar.bz2
